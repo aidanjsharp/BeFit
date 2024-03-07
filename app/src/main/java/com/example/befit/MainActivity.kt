8249d0a0894ae2b0a4138dac1eed6c374d4cdbec
aidanjsharp <aidanjsharp@hotmail.com>
2024-03-07 00:42:39 -0500

    BeFit label and forgot button

--- app/src/main/java/com/example/befit/MainActivity.kt
+++ app/src/main/java/com/example/befit/MainActivity.kt
@@ -3,6 +3,8 @@
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -16,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,14 +47,38 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 //        text = "Hello $name!",
 //        modifier = modifier
 //    )
-    Column {
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Box(){
+            Text(text = "BeFit.")
+        }
+        Column(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            UsernameField()
 
-        UsernameField()
+            PasswordField()
+        }
 
-        PasswordField()
-
-        Button(onClick = { /*onClick()*/ }){
-            Text("Next")
+        Column(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Button(onClick = { /*onClick()*/ }) {
+                Text("Log In")
+            }
+            Button(onClick = { /*onClick()*/ }) {
+                Text("Forgot Password")
+            }
         }
 
     }
