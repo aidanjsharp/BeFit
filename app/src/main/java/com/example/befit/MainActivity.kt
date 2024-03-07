c9a51b08c4271adae3a41399f51092a8bb5cd2b7
aidanjsharp <aidanjsharp@hotmail.com>
2024-03-06 23:24:11 -0500

    Basic Username/Pw and button

--- app/src/main/java/com/example/befit/MainActivity.kt
+++ app/src/main/java/com/example/befit/MainActivity.kt
@@ -1,14 +1,23 @@
-package com.example.befit
+                    package com.example.befit
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.befit.ui.theme.BeFitTheme
 
@@ -31,12 +40,46 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+    Column {
+
+        UsernameField()
+
+        PasswordField()
+
+        Button(onClick = { /*onClick()*/ }){
+            Text("Next")
+        }
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UsernameField() {
+    var text by remember { mutableStateOf("") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Username:") }
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PasswordField() {
+    var text by remember { mutableStateOf("") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Password:") }
+    )
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
